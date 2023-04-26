b68d369e0154d8d8b428290cce9e640916225447
HoroshoNaVolge <44640207+HoroshoNaVolge@users.noreply.github.com>
2023-04-26 09:57:25 +0300

    Corrected main

--- Program.java
+++ Program.java
@@ -1,13 +1,27 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 class Program {
     public static void main(String[] args) {
 
+        // Создаём игрушки через фабрику.
         ArrayList<Toy> toysForStock = ToyFactory.createToys(5);
+
         ToyStock stock = new ToyStock(toysForStock);
+
+        // Для наглядности, в ТЗ было условие создать метод установки веса.
+        stock.setToyWeight(1, 5);
+
         stock.play();
 
-        try{stock.getPrizeToy();}
-        catch (Exception ex){System.out.println("Нет призовой игрушки");};
+        try {
+            stock.getPrizeToy();
+            System.out.println("Результат розыгрыша записан в prize_toys.txt");
+        } catch (IOException ex) {
+            System.out.println("Произошла ошибка записи файла");
+        } finally {
+            System.out.println("Программа завершена.");
+        }
+
     }
 }
